cc9ba6688a26c6c8bfa5c2e7aac8f091aba630c9
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-04-21 21:40:29 -0600

    Commit changes to HTML CSS
    1. Added form to HTML
    2. Styled form and other elements on page
    3. Input more detailed content for end user

--- script/script.js
+++ script/script.js
@@ -7,8 +7,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("submit", writePassword);
+
+password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+})
