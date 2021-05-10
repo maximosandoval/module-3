10916acb5b0eeba0c73eabebf4b2542d52679ffc
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-05-10 14:39:16 -0600

    Fixed error with pop up window
    Pop up window was not working for some end users and working for others. 
    Added cleaner code for password generation

--- script/script.js
+++ script/script.js
@@ -1,131 +1,86 @@
 // Assignment Code
-//Assigned variables
 var generateBtn = document.querySelector("#generate");
-
-var upperChar = "ABCEDFGHIJKLMNOPQRSTUVWXYZ";
-
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-
-var specialChar = "!#$%^&*()+=<>_^&*~";
-
-var numberChar = "0123456789";
-
-//Empty variable to pass the value
-var passChar = "";
-
+//All the characters the user can choose from to create a password
+var lcCharacters = "abcdefghijklmnopqrstuvwxyz";
+var upCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numCharacters = "0123456789";
+var spCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+​
+ var newPassword = "";
+//var passChars = "";
+​
 // Write password to the #password input
-//If they come back true then they have to be placed into a useable string
-function userChoice() {
-  var length = prompt("Please select between 8-128");
-  if (length < 8 || length > 128) {
-    alert("Please select between 8-128");
-    return;
-  }
-  lowerCaseSelected = confirm("You want lower case?");
-  upperCaseSelected = confirm("You want upper case?");
-  specialCaseSelected = confirm("You want special case?");
-  numberCaseSelected = confirm("You want numbers?");
-  if (
-    !lowerCaseSelected &&
-    !upperCaseSelected &&
-    !specialCaseSelected &&
-    !numberCaseSelected
-  ) {
-    //Alert prompt
-    alert("You need to choose one.");
-    return;
-  }
-}
-
-//Assigns a password to the password box. Returns the function of generate password.
-//DO NOT ALTER SUPPLIED BY TEACHER
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+​
   passwordText.value = password;
-  //Write logic in here
-  //Check for true or false of all var's
-  var arrayFunction = function () {
-    if (lowerChar) {
-      finalArray = finalArray.concat(lowerChar);
-      console.log(lowerChar);
-    }
-    if (upperChar) {
-      finalArray = finalArray.concat(upperChar);
-    }
-    if (specialChar) {
-      finalArray = finalArray.concat(specialChar);
-    }
-    if (numberChar) {
-      finalArray = finalArray.concat(numberChar);
-    }
-    console.log(finalArray);
-    return finalArray;
-    {
-    }
-  };
 }
-
+​
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-function generateItems() {
-  //Lenght of end users password
-  var pLength = prompt("Select a password from 8-128 characters.");
-
-  //Password not proper length
-  if (pLength < 8 || pLength > 128) {
-    alert("Please select a password between 8-128 characters.");
-    return generateItems();
-  } else if (pLength >= 8 && pLength <= 128) {
-    //Insert all other confirms here
-    var lowerChar = confirm("Include lower case letters.");
-    var upperChar = confirm("Include upper case letters.");
-    var specialChar = confirm("Include special case letters.");
-    var numberChar = confirm("Include numbers.");
-  }
-  var userChoice = {
-    pLength,
-    numberChar,
-    lowerChar,
-    specialChar,
-    upperChar,
-  };
-  //Need to Return user choice
-  return userChoice;
-}
-
-// function randomChar(arr) {
-//   return arr[Math.floor(Math.random() * arr.length)];
-// }
-
-//Function create password
-//May have tochange slightly based on how functions are organized
-function createPassword(pWord) {
-  var anyNumber = Math.floor(Math.random() * pWord.length);
-  var anysymbol = pWord[anyNumber];
-  return anysymbol;
-}
-// Function generate password
+​
 function generatePassword() {
-  var userItems = userChoice();
-  var possibleCharacters = arrayFunction();
-  var password = "";
-
-  //JS array concat to join arrays
-  arrayFunction();
-  for (i = 0; i < userItems.pLength; i++) {
-    password = password.concat(randomChar(possibleCharacters));
+  //Setting up undefined variables for the new password
+  var newPassword = "";
+​
+  //Get the length of the password
+  var pLength = prompt(
+    "How many characters would you like to include in your password?"
+  );
+​
+  //If the password is not between 8 and 128 characters, return to the prompt
+  if (pLength < 8 || pLength > 128) {
+    alert("Password must be between 8 and 128 characters");
+    return;
+  } else if (pLength >= 8 && pLength <= 128) {
+    //Does the user want lowercase letters
+    var lowercaseNum = confirm("Include lowercase letters?");
+​
+    //If yes, add the characters to the password characters array
+    if (lowercaseNum) {
+      passChars = passChars + lcCharacters;
+      console.log(passChars);
+    }
+    //Does the user want uppercase letters
+    var uppercaseNum = confirm("Include upper case letters?");
+​
+    //if yes, add the characters to the password array
+    if (uppercaseNum) {
+      passChars = passChars + upCharacters;
+      console.log(passChars);
+    }
+​
+    //Does the user want numbers
+    var numCharNum = confirm("Include numbers?");
+​
+    //If yes, add it to the password array
+    if (numCharNum) {
+      passChars = passChars + numCharacters;
+      console.log(passChars);
+    }
+​
+    //Does the user want special characters
+    var specialNum = confirm("Include special characters?");
+​
+    //If yes, add it to the password array
+    if (specialNum) {
+      passChars = passChars + spCharacters;
+      console.log(passChars);
+    }
+​
+    //If the user doesnt choose any, prompt them, then start over
+    if (passChars === "") {
+      alert("You must select characters");
+    }
+​
+    //from the created array choose random items for the length that is given.
+    for (var i = 0; i < pLength; i++) {
+      newPassword += passChars.charAt(
+        Math.floor(Math.random() * passChars.length)
+      );
+    }
+    console.log(newPassword);
+    return newPassword; //Return the password
   }
-  console.log(password);
-  return generatePassword;
-}
-
-//Create a loop that attaches to generate password and pulls characters from the generate characters array. Create a function for password length. Need to break it out.
-
-//TO DO: We need to have new password loop/coomect with generate password
-
-//Event Listner for button click
-//generateBtn.addEventListener("click", writePassword);
-
-{
 }
